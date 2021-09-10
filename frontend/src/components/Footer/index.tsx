60d75c89e2feca8deb704998f80e29019f0d2ba9
Carlos Daniel <carlos_daniel_921@outlook.com>
2021-09-10 20:50:13 -0300

    correção footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
 
         <div>
-            <footer className="footer mt-auto py-3 bg-dark">
+            <footer className="footer mt-auto fixed-bottom py-3 bg-dark">
                 <div className="container">
                     <p className="text-light">App desenvolvido por <a href="https://github.com/CarlosDanielSantosOliveira" target="_blank" rel="noreferrer"> Carlos Daniel </a></p>
                     <p className="text-light"><small><strong>Semana Spring React</strong><br />
